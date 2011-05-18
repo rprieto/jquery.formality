09f33039369ed7ca256c36fd7d8da09633317ebf
Romain Prieto <romain.prieto.web@gmail.com>
2011-05-18 11:03:05 +1000

    Reorganised code in several modules (scaffolding, formValues, inputSet)

--- jquery.formality.js
+++ jquery.formality.js
@@ -1,7 +1,10 @@
 $.fn.extend(
   
   (function() {
-    
+        
+	//
+	// Utils
+	//
 	
     var reduce = function(list, fn, aggregate) {
       list.forEach(function(item) {
@@ -10,11 +13,88 @@ $.fn.extend(
       return aggregate;
     };
     
-        
-    var inputSet = function(inputs) {
+	//
+	// Structure of target inputs that mirrors the final hierarchy
+	//
+	//    elements: [
+	//      $input1, $input2
+	//    ],
+	//
+	//    classes: [
+	//      class1: { elements, classes, arrays },
+	//      class2: { elements, classes, arrays }
+	//    ],
+	//
+	//    arrays: [
+	//      array1: [
+	//        { elements, classes, arrays },
+	//        { elements, classes, arrays }
+	//      ],
+	//      array2: [
+	//        { elements, classes, arrays },
+	//        { elements, classes, arrays }
+	//      ]
+	//    ]
+	//  	
+      
+	var scaffolding = function() {
+		
+		var hierarchyLevel = function() {
+			return {
+				elements: [],
+				classes: {},
+				arrays: {}
+			};
+		};
+			
+	    var getAttributes = function($root, $input) {
+	    	var $usefulParents = $input.parents('*[data-formality-class]');
+	    	return $usefulParents.map(function(_, parent) {
+	    		return $(parent).attr('data-formality-class');
+	    	}).get().reverse();
+	    };
+	    
+	    var completeHierarchy = function(object, classes) {
+	    	var leaf = object;
+	    	reduce(classes, function(name, result) {
+	    		if (!(result.classes[name])) {
+	    			result.classes[name] = hierarchyLevel();
+	    		}
+	    		leaf = result.classes[name];
+	    		return leaf;
+	    	}, object);
+	    	return leaf;
+	    };
+	    
+	    var create = function($root) {	
+			var $inputs = $('input,select', $root);
+			
+			var hierarchy = hierarchyLevel();     
+			
+			$inputs.each(function(_, input) {
+				var attributes = getAttributes($root, $(input));
+				var leaf = completeHierarchy(hierarchy, attributes);
+				leaf.elements.push($(input));
+			});
+			
+			return hierarchy;
+	    };
+		      
+		return {
+			create: create	
+		};
+	          		
+	}; 
+
+	//
+	// Set of inputs at a given level that can return a flat javascript object
+	// The whole set must be processed at once since checkboxes for example generate an array
+	//
+	    
+    var inputSet = function() {
     		
 	    var getKey = function($item) {      
-	      return $item.attr('name') || $item.attr('id')
+	      return $item.attr('name') || $item.attr('id');
 	    };
 		
 	    var textsAndRadios = function($item, form) {
@@ -41,101 +121,51 @@ $.fn.extend(
 	      return form;
 	    };
     	
+    	var is = function(type) {
+    		return function(item) {
+    			return item.is(type);
+    		}
+    	};
+    	
     	return {
-    	  createObject: function() {
+    	  objectFromValues: function(inputs) {
 		      var object = {};   
-		      reduce(inputs.filter(function(item) { return item.is(':text') }), textsAndRadios, object);
-		      reduce(inputs.filter(function(item) { return item.is(':radio:checked') }), textsAndRadios, object);
-		      reduce(inputs.filter(function(item) { return item.is('select') }), selects, object);
-		      reduce(inputs.filter(function(item) { return item.is(':checkbox') }), checkboxes, object);
+		      reduce(inputs.filter(is(':text')), textsAndRadios, object);
+		      reduce(inputs.filter(is(':radio:checked')), textsAndRadios, object);
+		      reduce(inputs.filter(is('select')), selects, object);
+		      reduce(inputs.filter(is(':checkbox')), checkboxes, object);
 		      return object;      
 		    }
     	}
     		
     };    
-  
-	  
-	var defaultObject = function() {
-		return {
-			elements: [],
-			classes: {},
-			arrays: {}
-		};
-	};
-		
-    var getAttributes = function($root, $input) {
-    	var $usefulParents = $input.parents('*[data-formality-class]');
-    	return $usefulParents.map(function(_, parent) {
-    		return $(parent).attr('data-formality-class');
-    	}).get().reverse();
-    };
-    
-    var completeHierarchy = function(object, classes) {
-    	var leaf = object;
-    	reduce(classes, function(name, result) {
-    		if (!(result.classes[name])) {
-    			result.classes[name] = defaultObject();
-    		}
-    		leaf = result.classes[name];
-    		return leaf;
-    	}, object);
-    	return leaf;
-    };
-    
-    var processLevel = function(level, object) {
-    	var object = inputSet(level.elements).createObject();
-    	Object.keys(level.classes).forEach(function(name) {
-    		object[name] = processLevel(level.classes[name], object[name]);
-    	});
-    	return object;
-    };
-    
-    var formality = function() {
-      var $root = this;	
-      var $inputs = $('input,select', $root);
- 
-      var hierarchy = defaultObject();     
-      
-      $inputs.each(function(_, input) {
-      	 var attributes = getAttributes($root, $(input));
-		 var leaf = completeHierarchy(hierarchy, attributes);
-		 leaf.elements.push($(input));
-      });
-      
 
-      return processLevel(hierarchy);
-      
-      // for each input
-      //   traverse parents and get data-attributes
-      //     data-formality-class
-      //     data-formality-array
-            
-      //  build the final structure, with at each level
-      // 
-      //    elements: [
-      //      $input1, $input2
-      //    ],
-      //
-      //    classes: [
-      //      class1: { elements, classes, arrays },
-      //      class2: { elements, classes, arrays }
-      //    ],
-      //
-      //    arrays: [
-      //      array1: [
-      //        { elements, classes, arrays },
-      //        { elements, classes, arrays }
-      //      ],
-      //      array2: [
-      //        { elements, classes, arrays },
-      //        { elements, classes, arrays }
-      //      ]
-      //    ]      
-     
-    };
-    
+	
+	//
+	// Final object returned by the plugin
+	//
+	
+	var formValues = function($root) {
+    	
+	    var processLevel = function(level, object) {
+	    	var object = inputSet().objectFromValues(level.elements);
+	    	Object.keys(level.classes).forEach(function(name) {
+	    		object[name] = processLevel(level.classes[name], object[name]);
+	    	});
+	    	return object;
+	    };
+
+    	var structure = scaffolding().create($root);	        
+    	return processLevel(structure);   		
+    	
+	};
+
+	//
+	// Entry point:  $('#myForm').formality();
+	//
+	
     return {
-      formality: formality
+      formality: function() { return formValues(this); }
     }
     
   })()
