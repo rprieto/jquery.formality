8812a667314dd9f628234834250cbda6da239439
Romain Prieto <romain.prieto.web@gmail.com>
2011-05-18 23:06:03 +1000

    Removed obsolete test, and now using escaped newline litterals to make test HTML more legible

--- tests/test.textfields.js
+++ tests/test.textfields.js
@@ -1,24 +1,13 @@
 
 describe('text inputs', function() {
 
-  it('creates one entry per text input', function() {
-    var fixture = $(
-      '<form>' + 
-        '<input type="text" name="foo" />' + 
-        '<input type="text" name="bar" />' +
-      '</form>'
-    );
-    var form = fixture.formality();
-    expect(Object.keys(form).length).toBe(2);    
-  });
-
   it('uses the text input values as the object values', function() {
-    var fixture = $(
-      '<form>' + 
-        '<input name="foo" value="one" />' + 
-        '<input name="bar" value="two" />' +
-      '</form>'
-    );
+    var fixture = $(' \
+      <form> \
+        <input name="foo" value="one" /> \
+        <input name="bar" value="two" /> \
+      </form> \
+    ');
     var form = fixture.formality();
     expect(form).toEqual({
     	foo: 'one',
@@ -27,11 +16,11 @@ describe('text inputs', function() {
   });
   
   it('considers text inputs with no values as empty strings', function() {
-    var fixture = $(
-      '<form>' + 
-        '<input name="foo" />' + 
-      '</form>'
-    );
+    var fixture = $(' \
+      <form> \
+        <input name="foo" /> \
+      </form> \
+    ');
     var form = fixture.formality();
     expect(form).toEqual({
     	foo: ''
