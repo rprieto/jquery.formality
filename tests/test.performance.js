dd067dada088b5e2fd5683ccb9ab4d375fc5e21f
Romain Prieto <romain.prieto.web@gmail.com>
2011-05-28 13:08:50 +1000

    Performance test: - removed nested forms in the test data (not supported by firefox) - added a mix of basic and ONGL names

--- tests/test.performance.js
+++ tests/test.performance.js
@@ -11,7 +11,6 @@ describe('nested', function() {
   it('should process a big form in a reasonable time', function() {
   	
     var fixture = $(
-      '<form>' + 
         '<div id="countries">' +
           '<div class="country" data-formality-context="countries[0]">' +
 	        '<input type="text" name="name" value="Australia" />' +
@@ -32,16 +31,16 @@ describe('nested', function() {
 	          '<input type="text" name="name" value="Sydney" />' +
 	          '<input type="text" name="postcode" value="2000" />' +
 	          '<form>' +
-          	    '<input type="radio" name="visited" value="true" checked="checked" />' + 
-        	    '<input type="radio" name="visited" value="false" />' +        	        	        
+          	    '<input type="radio" name="visited" value="true" checked="checked" />' +
+        	    '<input type="radio" name="visited" value="false" />' +
         	  '</form>' +
 	        '</div>' +
 	        '<div data-formality-context="cities[1]">' +
 	          '<input type="text" name="name" value="Melbourne" />' +
 	          '<input type="text" name="postcode" value="3000" />' +
 	          '<form>' +
-          	    '<input type="radio" name="visited" value="true" checked="checked" />' + 
-        	    '<input type="radio" name="visited" value="false" />' +        	        	        
+          	    '<input type="radio" name="visited" value="true" checked="checked" />' +
+        	    '<input type="radio" name="visited" value="false" />' +
         	  '</form>' +
 	        '</div>' +
 	      '</div>' +
@@ -55,29 +54,22 @@ describe('nested', function() {
 	            '<input type="checkbox" name="type" value="Monarchy" />' + 
 	          '</div>'	+        
 	          '<input type="text" name="president" value="Nicolas Sarkozy" />' +
-		      '<div data-formality-context="ministers">' +
-		        '<input type="text" name="primeMinister" value="Francois Fillon" />' +
-		      '</div>' +
+  	          '<input type="text" name="ministers.primeMinister" value="Francois Fillon" />' +
 	        '</div>' +
-	        '<div data-formality-context="cities[0]">' +
-	          '<input type="text" name="name" value="Paris" />' +
-	          '<input type="text" name="postcode" value="75001" />' +
-	          '<form>' +
-          	    '<input type="radio" name="visited" value="true" checked="checked" />' + 
-        	    '<input type="radio" name="visited" value="false" />' +        	        	        
-        	  '</form>' +
-	        '</div>' +
-	        '<div data-formality-context="cities[1]">' +
-	          '<input type="text" name="name" value="Lyon" />' +
-	          '<input type="text" name="postcode" value="69001" />' +
-	          '<form>' +
-          	    '<input type="radio" name="visited" value="true" />' + 
-        	    '<input type="radio" name="visited" value="false"  checked="checked" />' +        	        	        
-        	  '</form>' +
-	        '</div>' +
+	        '<input type="text" name="cities[0].name" value="Paris" />' +
+	        '<input type="text" name="cities[0].postcode" value="75001" />' +
+	        '<form>' +
+          	  '<input type="radio" name="cities[0].visited" value="true" checked="checked" />' +
+        	  '<input type="radio" name="cities[0].visited" value="false" />' +
+        	'</form>' +
+	        '<input type="text" name="cities[1].name" value="Lyon" />' +
+	        '<input type="text" name="cities[1].postcode" value="69001" />' +
+	        '<form>' +
+          	  '<input type="radio" name="cities[1].visited" value="true" />' +
+        	  '<input type="radio" name="cities[1].visited" value="false"  checked="checked" />' +
+        	'</form>' +
 	      '</div>' +
-	  	'</div>' +        
-      '</form>'
+	  	'</div>'
     );
     
     var world;
