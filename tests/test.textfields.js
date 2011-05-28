f76408210c33da292c5f06b4f9b227ef76fe221d
Romain Prieto <romain.prieto.web@gmail.com>
2011-05-28 16:23:16 +1000

    New test for text fields with quotes / double quotes in the value

--- tests/test.textfields.js
+++ tests/test.textfields.js
@@ -15,7 +15,7 @@ describe('text inputs', function() {
     	bar: 'two'
     });
   });
-  
+
   it('considers text inputs with no values as empty strings', function() {
     var fixture = $(
       '<form>' +
@@ -27,6 +27,19 @@ describe('text inputs', function() {
     	foo: ''
     });
   });
-  
+
+  it('can handle text fields with quotes and double quotes in the value', function() {
+    var fixture = $(
+      '<form>' +
+        '<input name="foo" />' +
+      '</form>'
+      );
+    fixture.find('input').val('\'Hello\' \"bob\"');
+    var form = fixture.formality();
+    expect(form).toEqual({
+    	foo: '\'Hello\' \"bob\"'
+    });
+  });
+
 });
 
