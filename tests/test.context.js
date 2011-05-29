7cd5a17965114aed88aaad88a6bc697c7d6c1a32
Romain Prieto <romain.prieto.web@gmail.com>
2011-05-30 00:25:25 +1000

    New FAILING test case: ONGL paths within contexts

--- tests/test.context.js
+++ tests/test.context.js
@@ -30,6 +30,28 @@ describe('context', function() {
   });
 
   it('understands ONGL-type paths in the context attribute', function() {
+    var fixture = $(
+      '<form>' +
+        '<input type="text" name="name" value="Bob" />' +
+	    '<div data-formality-context="address.city">' +
+	        '<input type="text" name="name" value="Sydney" />' +
+	        '<input type="text" name="postcode" value="2000" />' +
+	      '</div>' +
+        '</div>' +
+      '</form>'
+    );
+
+    var country = fixture.formality();
+
+    expect(country).toEqual({
+    	name: 'Bob',
+    	address: {
+    	  city: {
+    	  	name: 'Sydney',
+    	  	postcode: '2000'
+    	  }
+    	}
+    });
   });
     
 });
